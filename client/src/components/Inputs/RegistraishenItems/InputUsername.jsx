7ccf0bd78d7f634c9d686f74eae47cde0eee8af4
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2023-07-09 18:24:20 +0300

    deleted items

--- client/src/components/Inputs/RegistraishenItems/InputUsername.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function InputUsername() {
-    return (
-        <div>
-            <input type="text" placeholder='Username'/>
-        </div>
-    )
-}
