6d1ed04c09e57104bb6509ba2f058de775e12393
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2023-07-10 00:14:05 +0300

    edited the file

--- client/src/pages/Page/Shop.jsx
+++ client/src/pages/Page/Shop.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Footer from 'section/Footer'
 import Header from 'section/Header'
-import ShopInfo from 'section/ShopInfo'
+import ShopInfo from 'section/sectionInfo/ShopInfo'
 
 export default function Shop() {
     return (
