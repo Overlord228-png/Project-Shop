afecce8385af73266f7d14968f3d4049b0dfaefe
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2023-07-10 13:46:48 +0300

    edited file

--- client/src/components/Inputs/LogInItems/InputPasswordItem.jsx
+++ client/src/components/Inputs/LogInItems/InputPasswordItem.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 export default function InputPasswordItem(props) {
     return (
         <div>
-            <input placeholder='password' type="password" id="password" value={props.password} onChange={(e) =>props.setPassword(e.target.value)} required />
+            <input
+                className="login__input-password"
+                placeholder='password'
+                type="password"
+                id="password"
+                value={props.password}
+                onChange={(e) =>props.setPassword(e.target.value)}
+                required
+            />
         </div>
     )
 }
