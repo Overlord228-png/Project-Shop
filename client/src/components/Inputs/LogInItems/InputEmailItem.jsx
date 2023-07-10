8be62aa99d3d781206fe2fdd5a6d1e9b9100da54
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2023-07-10 13:48:10 +0300

    edited file

--- client/src/components/Inputs/LogInItems/InputEmailItem.jsx
+++ client/src/components/Inputs/LogInItems/InputEmailItem.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function InputEmailItem(props) {
     return (
-        <div>
-            <input placeholder='email' type="email" id="email" value={props.email} onChange={(e) =>props.setEmail(e.target.value)} required />
-        </div>
+        <input
+            className="login__input-email"
+            placeholder='email'
+            type="email"
+            id="email"
+            value={props.email}
+            onChange={(e) =>props.setEmail(e.target.value)} 
+            required 
+        />
     )
 }
