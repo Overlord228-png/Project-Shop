566e0c4bf02c3b8355f342ec478febdf0504a5fc
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2023-07-10 13:49:01 +0300

    edited file

--- client/src/components/Inputs/LogInItems/InputCheckboxItem.jsx
+++ client/src/components/Inputs/LogInItems/InputCheckboxItem.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function InputCheckboxItem(props) {
     return (
-        <div>
-            <input type="checkbox" id="remember" checked={props.remember} onChange={(e) =>props.setRemember(e.target.checked)} />
-        </div>
+        <input
+            type="checkbox"
+            id="remember" 
+            className="login-checkbox"
+            checked={props.remember} 
+            onChange={(e) =>props.setRemember(e.target.checked)} 
+        />
     )
 }
