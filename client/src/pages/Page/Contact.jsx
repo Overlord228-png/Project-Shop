f198b5432f0e01defcaaabc2651bcb68359289d2
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2023-07-10 13:36:47 +0300

    add import file ContantInfo

--- client/src/pages/Page/Contact.jsx
+++ client/src/pages/Page/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Footer from 'section/Footer'
 import Header from 'section/Header'
-import ContactInfo from 'section/ContactInfo'
+import ContactInfo from 'section/sectionInfo/ContactInfo'
 
 export default function Contact() {
     return (
