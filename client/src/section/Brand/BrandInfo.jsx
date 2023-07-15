de3e4fb8eb1bf019ac02b1dc0b544dccb9245c39
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2023-07-15 10:50:20 +0300

    add useState and useEfect for brands

--- client/src/section/Brand/BrandInfo.jsx
+++ client/src/section/Brand/BrandInfo.jsx
@@ -1,70 +1,43 @@
-import React from "react"
+import BtnAddBrand from "components/Buttons/BtnAddBrand"
+import InputNameBrand from "components/Inputs/InputAdd/InputNameBrand"
+import React, { useEffect, useState } from "react"
+import axios from "axios";
 
-export default function ProductInfo() {
+export default function BrandInfo() {
+    const [saveIdBrand, setSeaveIdBrand] = useState()
+    const [brandName, setBrandName] = useState ('')
+    const [brands, setBrands] = useState([])
+
+    const ShowIdBrand =(id)=>{
+        setSeaveIdBrand(id)
+    }
+
+    const loadBrands = async () => {
+        const resp = await axios.get('http://localhost:5000/api/brand')
+        setBrands(resp.data)
+    }
+
+    const addBrand = async () => {
+        const resp = await axios.post('http://localhost:5000/api/brand', {name:brandName})
+        setBrands([...brands,resp.data])
+    }
+
+    useEffect(()=>{
+        loadBrands()
+    },[])
     return (
-        <></>
+        <div className="">
+            <InputNameBrand setBrandName={setBrandName} brandName={brandName} />
+            <BtnAddBrand addBrand={addBrand} />
+            <ul onChange={(e)=> ShowIdBrand(e.target.selectedOptions[0].value)}>
+                {
+                    brands.map((brand,idx)=>
+                        <li key={idx} value={brand.id}>
+                            {brand.name}
+                        </li>
+                    )
+                }
+            </ul>
+        </div>
     )
 }
-
-/**
-<div>
-<div className="">
-    <InputNameProduct setNameDevice={setNameDevice} nameDevice={nameDevice}/>
-    <InputPriceProduct setPriceDevice={setPriceDevice} priceDevice={priceDevice}/>
-</div>
-
-<br />
-<br />
-
-<div className="">
-    <InputAddFile setImg={setImg} img={img} />
-</div>
-
-<br />
-<br />
-
-<div className="">
-    <select onChange={(e)=>ShowIdType(e.target.selectedOptions[0].value)}>
-        {
-            types.map((type,idx)=>
-            <option key={idx} value={type.id}>
-                {type.name}
-            </option>
-            )
-        }
-        <option value="1">sad</option>
-    </select>
-</div>
-
-<br />
-<br />
-
-<div className="">
-    <select onChange={(e)=> ShowIdBrand(e.target.selectedOptions[0].value)}>
-        {
-            brands.map((brand,idx)=>
-            <option key={idx} value={brand.id}>
-                {brand.name}
-            </option>
-            )
-        }
-        <option value="1">sad</option>
-    </select>
-</div>
-
-<br />
-<br />
-
-<div className="">
-    <AddProduct addProduct={addProduct} />
-</div>
-
-{showProduct.map((product) => (
-    <div key={product.id}>
-        <h2>{product.name}</h2>
-        <img src={`http://127.0.0.1:5000/${product.img}`} alt={product.name}/>
-        <h2>{product.price}</h2>
-    </div>
-))}
-</div>
- */
