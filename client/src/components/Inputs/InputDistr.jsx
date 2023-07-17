8a28159b44183c1d2e57032c744ab6baa91fb537
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2023-07-17 12:38:23 +0300

    добавил инпкт для рассылки

---
+++ client/src/components/Inputs/InputDistr.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function InputDistr() {
+    return (
+        <input 
+            type="text" 
+            placeholder='Введите свой E-mail адрес'
+            className=''
+        />
+    )
+}
