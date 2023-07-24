0755365f61e7093220496061b259fa4772320acd
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2023-07-24 20:33:50 +0300

    editing file

--- client/src/section/Footer.jsx
+++ client/src/section/Footer.jsx
@@ -1,26 +1,43 @@
 import BtnDistr from "components/Buttons/BtnDistr"
 import InputDistr from "components/Inputs/InputDistr"
 import React from "react"
+import Telegram from "img/telegram.png"
+import Twitter from "img/twitter.png"
+import { NavLink } from "react-router-dom"
 import "style/footer.css"
 
 export default function Footer() {
     return (
         <footer className="footer">
             <div className="footer__conteiner">
-                <div className="footer__wrapper">
-                    <div className="distribution">
-                        <h1>Подпишитесь на E-mail рассылку</h1>
-                        <h6>
-                            Если хотиет быть в курсе последних новостей и новинок кино 
-                            - заполните форму ниже и оформите бесплатную E-mail рассылку!
-                        </h6>
-                        <div className="">
-                            <InputDistr />
-                            <BtnDistr />
-                        </div>
+                <div className="distribution">
+                    <h1 className="text__title">Подпишитесь на E-mail рассылку</h1>
+                    <p className="specification__text">
+                        Если хотиет быть в курсе последних новостей и новинок кино 
+                        - заполните форму ниже и оформите бесплатную E-mail рассылку!
+                    </p>
+                    <div className="distr__block">
+                        <InputDistr />
+                        <BtnDistr />
                     </div>
-                    <p>09/07/2023 - по настоящие время © Nikita Vavilov.  Все права защищены</p>
-                    <p>Политика конфиденциальности</p>
+                </div>
+                <div className="text__block">
+                    <p className="text__start">09/07/2023 - по настоящие время © Nikita Vavilov.  Все права защищены</p>
+                    <p className="text__private">Политика конфиденциальности</p>
+                </div>
+                <div className="">
+                    <ul>
+                        <li>
+                            <NavLink >
+                                <img src={Telegram} alt="" />
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink >
+                                <img src={Twitter} alt="" />
+                            </NavLink>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </footer>
