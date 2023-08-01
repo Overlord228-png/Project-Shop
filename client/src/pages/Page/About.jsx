14cefc07ccff73bf2bbda5429a260288f2808e26
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2023-08-01 22:46:32 +0300

    edited file page

--- client/src/pages/Page/About.jsx
+++ client/src/pages/Page/About.jsx
@@ -7,9 +7,7 @@ export default function About() {
     return (
         <>
             <Header />
-            <br />
             <AboutInfo />
-            <br />
             <Footer/>
         </>
     )
