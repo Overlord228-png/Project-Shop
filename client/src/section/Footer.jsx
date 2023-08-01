df6cb71d29c9fa65d7327e2de80b878b90112949
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2023-08-01 22:40:23 +0300

    edited file page

--- client/src/section/Footer.jsx
+++ client/src/section/Footer.jsx
@@ -1,9 +1,14 @@
+import React from "react"
+import { NavLink } from "react-router-dom"
+
 import BtnDistr from "components/Buttons/BtnDistr"
 import InputDistr from "components/Inputs/InputDistr"
-import React from "react"
-import Telegram from "img/telegram.png"
-import Twitter from "img/twitter.png"
-import { NavLink } from "react-router-dom"
+
+import Linkedin from "img/free-icon-linkedin-2504923.png"
+import Twitter from "img/free-icon-twitter-2504947.png"
+import Telegram from "img/free-icon-telegram-2504941.png"
+import GitHub from "img/free-icon-github-270798.png"
+
 import "style/footer.css"
 
 export default function Footer() {
@@ -22,19 +27,29 @@ export default function Footer() {
                     </div>
                 </div>
                 <div className="text__block">
-                    <p className="text__start">09/07/2023 - по настоящие время © Nikita Vavilov.  Все права защищены</p>
+                    <p className="text__start">09/07/2023 - по настоящие время © Nikita Vavilov. Все права защищены</p>
                     <p className="text__private">Политика конфиденциальности</p>
                 </div>
                 <div className="">
                     <ul>
                         <li>
                             <NavLink >
-                                <img src={Telegram} alt="" />
+                                <img src={Linkedin} alt="Linkedin" />
                             </NavLink>
                         </li>
                         <li>
                             <NavLink >
-                                <img src={Twitter} alt="" />
+                                <img src={Twitter} alt="Twitter" />
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink >
+                                <img src={Telegram} alt="Telegram" />
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="https://github.com/Overlord228-png">
+                                <img src={GitHub} alt="GitHub"/>
                             </NavLink>
                         </li>
                     </ul>
