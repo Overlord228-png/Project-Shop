232415119447addb0cd74f82cccec40ad546c41b
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2023-08-01 22:48:07 +0300

    edited file page

--- client/src/pages/Page/Main.jsx
+++ client/src/pages/Page/Main.jsx
@@ -6,9 +6,7 @@ export default function Main() {
     return (
         <>
             <Header />
-            <br />
             <MainInfo />
-            <br />
             <Footer />
         </>
     )
