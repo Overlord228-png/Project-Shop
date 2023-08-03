4c9eb216cfdba401d52ea4049b18a8ae38c81df0
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2023-08-03 22:58:30 +0300

    edited file page

--- client/src/store/features/userSlice.js
+++ client/src/store/features/userSlice.js
@@ -1,28 +1,64 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from 'axios'
+import jwtDecode from 'jwt-decode'
+
+export const registration = createAsyncThunk('registration/user', async(account) => {
+    try {
+        const resp = await axios.post('http://127.0.0.1:5000/api/user/registration',account)
+        return resp.data.token
+    } catch(e) {
+        return null
+    }
+})
+
+export const aut = createAsyncThunk('auth/user', async (account) => {
+    try {
+        const resp = await axios.post('http://127.0.0.1:5000/api/user/aut', account);
+        return resp.data.token;
+    } catch (e) {
+        return null;
+    }
+});
 
 const userSlice = createSlice({
     name:'user',
     initialState: {
-        id: number,
+        id: -1,
         email: null,
-        role: null
+        role: null,
+        errorLoading: false
     },
-    reducers: {
-        changeId: (state,action) => {
-            state.name = action.payload;
-        },
-        changeAge: (state,action) => {
-            state.age = action.payload;
-        },
-        changeCity: (state,action) => {
-            state.city = action.payload;
-        },
-        changeNunber: (state,action) => {
-            state.number = action.payload;
-        }
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(registration.fulfilled, (state,action)=>{
+            if (action.payload) {
+                state.errorLoading = false
+                localStorage.setItem('token',action.payload)
+                const decode = jwtDecode(action.payload)
+                state.email = decode.email
+                state.role = decode.role
+                state.id = decode.id
+                console.log(decode)
+            } else {
+                state.errorLoading = true
+            }
+        })
+        .addCase(aut.fulfilled, (state, action) => {
+            if (action.payload) {
+                state.errorLoading = false;
+                localStorage.setItem('token', action.payload);
+                const decode = jwtDecode(action.payload);
+                state.email = decode.email;
+                state.role = decode.role;
+                state.id = decode.id;
+                console.log(decode);
+            } else {
+                state.errorLoading = true;
+            }
+        })
     }
 })
 
 export default userSlice.reducer
 
-export const {changeName,changeAge,changeCity,changeNunber} = userSlice.actions
+export const {} = userSlice.actions
